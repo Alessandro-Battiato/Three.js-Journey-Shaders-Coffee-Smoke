6472beddd2c4ec003c3c2ffd9972b53a00ea4fd9
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-01 17:41:43 +0200

    fix: fixed smoke direction, animation speed, and turned repeat wrapping on as to let the animation loop

--- src/script.js
+++ src/script.js
@@ -91,12 +91,15 @@ smokeGeometry.scale(1.5, 6, 1.5);
 
 // Perlin texture
 const perlinTexture = textureLoader.load('./perlin.png');
+perlinTexture.wrapS = THREE.RepeatWrapping;
+perlinTexture.wrapT = THREE.RepeatWrapping;
 
 // Material
 const smokeMaterial = new THREE.ShaderMaterial({
     vertexShader: coffeeSmokeVertexShader,
     fragmentShader: coffeeSmokeFragmentShader,
     uniforms: {
+        uTime: new THREE.Uniform(0),
         uPerlinTexture: new THREE.Uniform(perlinTexture) // replaces the so far used { value: perlinTexture } syntax
     },
     side: THREE.DoubleSide,
@@ -116,7 +119,10 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update smoke
+    smokeMaterial.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update()
--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -1,13 +1,20 @@
+uniform float uTime;
 uniform sampler2D uPerlinTexture;
 
 varying vec2 vUv;
 
 void main() {
+    // Scale and animate
+    vec2 smokeUv = vUv;
+    smokeUv.x *= 0.5; // picks half of the texture on the x axis
+    smokeUv.y *= 0.3; // picks 0.3 of the texture on the y axis
+    smokeUv.y -= uTime * 0.03;
+
     // Smoke
-    float smoke = texture(uPerlinTexture, vUv).r; // retrieve only the R channel out of the RGB
+    float smoke = texture(uPerlinTexture, smokeUv).r; // retrieve only the R channel out of the RGB
 
     // Final color
-    gl_FragColor = vec4(smoke, smoke, smoke, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
