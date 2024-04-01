aecd48510c7cd9d4d2e7500eec1d0a43c1cf6270
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-04-01 16:40:10 +0200

    feat: perlin texture imported and vUv implementation

--- src/script.js
+++ src/script.js
@@ -89,11 +89,18 @@ const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64);
 smokeGeometry.translate(0, 0.5, 0);
 smokeGeometry.scale(1.5, 6, 1.5);
 
+// Perlin texture
+const perlinTexture = textureLoader.load('./perlin.png');
+
 // Material
 const smokeMaterial = new THREE.ShaderMaterial({
     vertexShader: coffeeSmokeVertexShader,
     fragmentShader: coffeeSmokeFragmentShader,
-    wireframe: true
+    uniforms: {
+        uPerlinTexture: new THREE.Uniform(perlinTexture) // replaces the so far used { value: perlinTexture } syntax
+    },
+    side: THREE.DoubleSide
+    // wireframe: true
 })
 
 // Mesh
--- src/shaders/coffeeSmoke/fragment.glsl
+++ src/shaders/coffeeSmoke/fragment.glsl
@@ -1,6 +1,8 @@
+varying vec2 vUv;
+
 void main() {
     // Final color
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
